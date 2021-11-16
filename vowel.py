99b620f922b0fe46445cd4e2cd361b0e2ed39186
MCA <febakjohnykutty2001@gmail.com>
2021-11-16 10:43:49 +0530

    1st commit

---
+++ vowel.py
@@ -0,0 +1,11 @@
+word=input("enter the sentence \n")
+vowels=['a','e','i','o''u']
+vow=[let for let in word if let in vowels]
+print(vow)
+
+
+output
+
+enter the sentence 
+shifana
+['i', 'a', 'a']
